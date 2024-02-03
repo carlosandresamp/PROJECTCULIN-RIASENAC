d39154759f855bc2d2ef64b23fdd8c24677213be
Carlos André <149840865+carlosandresamp@users.noreply.github.com>
2024-02-02 21:27:15 -0300

    Server

--- server/src/routes.js
+++ server/src/routes.js
@@ -5,12 +5,18 @@ import UserController from './Controller/UserController';
 import CategoryController from './Controller/CategoryController';
 import AvaliationController from './Controller/AvaliationController';
 import CommentController from './Controller/CommentController';
+// import uploads from './config/uploads';
+// import multer from 'multer';
+
+// const upload =  multer(uploads).single('image'); // pega a imagem do formulário
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
     return res.json({message: 'ok'});
 });
+
+// routes.post('/recipe', upload, RecipeController.Store)
 
 routes.get('/recipe', RecipeController.store)
 
