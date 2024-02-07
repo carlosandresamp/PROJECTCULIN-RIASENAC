da3e1984f348ae09e32e23c217056457b28a8247
carlosaugusto2023 <150488327+carlosaugusto007@users.noreply.github.com>
2024-02-07 17:28:00 -0300

    Update validar_senha.js

--- js/libs/validar_senha.js
+++ js/libs/validar_senha.js
@@ -1,5 +1,3 @@
-// Arquivo: validacao_senhas.js
-
 window.onload = function () {
     // Selecione os campos de senha, confirmação de senha, CPF e e-mail
     const senhaInput = document.querySelector('input[type="password"][placeholder="Crie uma senha"]');
@@ -9,34 +7,23 @@ window.onload = function () {
 
     // Adicione um ouvinte de evento de submit ao formulário
     document.querySelector('form').addEventListener('submit', function (event) {
-        // Verifique se as senhas são diferentes
-        if (senhaInput.value !== confirmarSenhaInput.value) {
-            // Impede o envio do formulário
-            event.preventDefault();
-            // Limpa os valores dos campos de senha e confirmação de senha
+      
+        if (senhaInput.value !== confirmarSenhaInput.value) {        
+            event.preventDefault();        
             senhaInput.value = '';
-            confirmarSenhaInput.value = '';
-            // Informa ao usuário que as senhas não correspondem
+            confirmarSenhaInput.value = '';         
             alert('As senhas não correspondem. Por favor, insira novamente.');
         }
-
         // Verifique se o CPF tem 11 dígitos
-        if (cpfInput.value.length !== 11) {
-            // Impede o envio do formulário
-            event.preventDefault();
-            // Limpa o valor do campo de CPF
+        if (cpfInput.value.length !== 11) {     
+            event.preventDefault();         
             cpfInput.value = '';
-            // Informa ao usuário que o CPF deve ter 11 dígitos
             alert('O CPF deve conter 11 dígitos. Por favor, insira novamente.');
         }
-
         // Verifique se o e-mail é do tipo @gmail.com
         if (!emailInput.value.endsWith('@gmail.com')) {
-            // Impede o envio do formulário
             event.preventDefault();
-            // Limpa o valor do campo de e-mail
             emailInput.value = '';
-            // Informa ao usuário que o e-mail deve ser do tipo @gmail.com
             alert('Por favor, insira um e-mail válido do tipo @gmail.com.');
         }
     });
