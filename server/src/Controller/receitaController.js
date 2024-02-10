a421610e9a2350d47d0b50a08992e8cb938316bf
carlosaugusto2023 <150488327+carlosaugusto007@users.noreply.github.com>
2024-02-10 11:07:19 -0300

    Update receitaController.js

--- server/src/Controller/receitaController.js
+++ server/src/Controller/receitaController.js
@@ -1,44 +1,35 @@
-import Receita from "../Model/receitas";
+// Controller
+import Receita from "../Model/Receita";
 import User from "../Model/User";
-class cadastroReceita {
+
+class CadastroReceita {
   async store(req, res) {
     try {
       const foto = req.file ? req.file.filename : null;
-      const { Titulo, nomeDoChef, ingredientes, modoDePreparo, tempo } =
-        req.body;
-      const { user_id } = req.headers;
-      if (
-        !req.body.Titulo ||
-        !req.body.nomeDoChef ||
-        !req.body.ingredientes ||
-        !req.body.modoDePreparo ||
-        !req.body.tempo
-      ) {
+      const { Titulo, ingredientes, modoDePreparo, tempo } = req.body;
+      const { username } = req.headers;
+
+      if (!Titulo || !ingredientes || !modoDePreparo || !tempo) {
         return res
           .status(400)
           .json({ message: "Todos os campos são obrigatórios." });
       }
 
-      console.log(Titulo, nomeDoChef, ingredientes, modoDePreparo, tempo);
-      console.log(user_id);
-      const userExists = await User.findById(user_id);
+      const userExists = await User.findOne({ username });
 
-      console.log(userExists);
       if (!userExists) {
         return res.status(404).json({ message: "Usuário não encontrado." });
       }
 
       const receitaCadastrada = await Receita.create({
         Titulo,
-        nomeDoChef,
         ingredientes,
         modoDePreparo,
         tempo,
-        user: user_id,
+        user: userExists._id,
         foto,
       });
 
-      // console.log(receitaCadastrada);
       return res.status(201).json({
         message: "Receita adicionada com sucesso.",
         receita: receitaCadastrada,
@@ -49,37 +40,18 @@ class cadastroReceita {
     }
   }
 
-  async show(req, res) {
-    try {
-      const { user_id } = req.headers;
-      console.log("headers", user_id);
-
-      if (!user_id) {
-        return res.status(404).json({ Message: "Usuário Não identificado!" });
-      }
-
-      const userRecieved = await User.findById({ user_id });
-
-      if (userRecieved) {
-        console.log("if com user", userRecieved);
-        let receitaUsuario = await Receita.findOne({ user: user_id });
-        return res.status(200).json(receitaUsuario);
-      }
-      console.log("else do false", userRecieved);
-      return res
-        .status(404)
-        .json({ Message: "Não existe usuário com este ID" });
-    } catch (error) {
-      return res.status(500).json({ Message: error.message });
-    }
-  }
-
   async index(req, res) {
     const { id } = req.query;
+
     if (!id) {
-      return res.status(404).json({ Message: "Insira um ID Válido" });
+      return res.status(404).json({ message: "Insira um ID Válido" });
     }
+
     const receitaSolicitada = await Receita.findById(id);
+
+    if (!receitaSolicitada) {
+      return res.status(404).json({ message: "Receita não encontrada." });
+    }
 
     return res.status(200).json(receitaSolicitada);
   }
@@ -89,9 +61,86 @@ class cadastroReceita {
       const receitas = await Receita.find();
       return res.json(receitas);
     } catch (error) {
-      return res.status(500).json({ Message: error.message });
+      console.error(error);
+      return res.status(500).json({ message: "Erro ao listar as receitas." });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.query;
+      const { Titulo, ingredientes, modoDePreparo, tempo } = req.body;
+      const { username } = req.headers;
+      console.log(id);
+      // console.log(req.body);
+      // console.log(Titulo);
+      // console.log(ingredientes);
+      // console.log(modoDePreparo);
+      // console.log(tempo);
+      // console.log(username);
+      const userExists = await User.findById(username);
+      // console.log(userExists);
+
+      if (!userExists) {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+      }
+
+      if (!Titulo || !ingredientes || !modoDePreparo || !tempo) {
+        return res
+          .status(400)
+          .json({ message: "Todos os campos são obrigatórios." });
+      }
+      const buscaReceita = Receita.findById(id);
+      console.log("busca receita:  ", buscaReceita);
+      const receitaAtualizada = await Receita.findOneAndUpdate(
+        { _id: id }, // filter
+        { Titulo, ingredientes, modoDePreparo, tempo }
+      );
+      console.log(receitaAtualizada);
+
+      if (!receitaAtualizada) {
+        return res.status(404).json({ message: "Receita não encontrada." });
+      }
+
+      return res.status(200).json({
+        message: "Receita atualizada com sucesso.",
+        receita: receitaAtualizada,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Erro ao atualizar a receita." });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params; // ID da receita a ser deletada
+      const { username } = req.headers;
+
+      const userExists = await User.findOne({ username });
+
+      if (!userExists) {
+        return res.status(404).json({ message: "Usuário não encontrado." });
+      }
+
+      const receitaDeletada = await Receita.findOneAndDelete({
+        _id: id,
+        user: userExists._id,
+      });
+
+      if (!receitaDeletada) {
+        return res.status(404).json({ message: "Receita não encontrada." });
+      }
+
+      return res.status(200).json({
+        message: "Receita deletada com sucesso.",
+        receita: receitaDeletada,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Erro ao deletar a receita." });
     }
   }
 }
 
-export default new cadastroReceita();
+export default new CadastroReceita();
