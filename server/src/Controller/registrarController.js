2583d540ce1d1ed95ec1214cbd08f5a3c9918dfa
carlosaugusto2023 <150488327+carlosaugusto007@users.noreply.github.com>
2024-02-10 11:12:11 -0300

    Update registrarController.js

--- server/src/Controller/registrarController.js
+++ server/src/Controller/registrarController.js
@@ -2,7 +2,7 @@ import Usuario from "../Model/User";
 
 class RegistrarUsuarioController {
   async store(req, res) {
-    const { nome, email, senha } = req.body;
+    const { nome, email, senha, confirmarsenha, cpf } = req.body;
 
     const usuarioExiste = await Usuario.findOne({ email });
     console.log(usuarioExiste);
@@ -10,7 +10,7 @@ class RegistrarUsuarioController {
       return res.status(409).send("Usuário já existe");
     }
 
-    await Usuario.create({ nome, email, senha });
+    await Usuario.create({ nome, email, senha, confirmarsenha, cpf });
 
     res.status(201).send("Usuário registrado com sucesso");
   }
